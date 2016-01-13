20b62f53d6ff45cd15e3d00c96dd4370fafccb75
Katie Fritz <katie@katiemfritz.com>
2016-01-13 10:24:18 -0500

    delete unneeded files

--- app.rb
+++ app.rb
@@ -7,8 +7,9 @@ class MyWebApp < Sinatra::Base
     erb :search
   end
   get '/results' do
-    @opac_records = opac("octavia butler")
-    @ku_records = ku("octavia butler")
+    keyword = params[:keyword]
+    @opac_records = opac("#{keyword}")
+    @ku_records = [] #ku("#{keyword}")
     erb :results
   end
 
@@ -18,16 +19,32 @@ class MyWebApp < Sinatra::Base
       @opac_url = "http://opac.cadl.org/search/X?SEARCH=#{keyword}&m=t&searchscope=15&a=&l=&Da=&Db=&p=&SORT=D"
       page = RestClient.get(@opac_url)
       html = Nokogiri::HTML(page)
-      @opac_count = html.css("td.browseHeaderData").first.text[/(\d+.*)\)/, 1]
-      html.css("div.briefcitDetails").map do |book|
-        title = book.css("h2.briefcitTitle a").first
-        author = book.css(".briefcitAuthor").first
-        link = book.css(".briefcitActions a").first
-        {
-          title: title.text,
-          author: author.text.gsub("\n", ""),
-          link: link.attr('href')
-        }
+      if html.css("div.briefcitDetails").any?
+        @opac_count = html.css("td.browseHeaderData").first.text[/(\d+.*)\)/, 1]
+        html.css("div.briefcitDetails").map do |book|
+          title = book.css("h2.briefcitTitle a").first
+          author = book.css(".briefcitAuthor").first
+          link = book.css(".briefcitActions a").first
+          {
+            title: title.text,
+            author: author.text.gsub("\n", ""),
+            link: link.attr('href')
+          }
+        end
+      elsif html.css("div.bibDisplayContentMain").any?
+        @opac_count = "One"
+        html.css("div.bibDisplayContentMain").map do |book|
+          title = book.css("td.bibInfoData").first
+          author = book.css("td.bibInfoData a").first
+          link = book.css("table.bibLinks a").first
+          {
+            title: title.text,
+            author: author.text.gsub("\n", ""),
+            link: link.attr('href')
+          }
+        end
+      else
+        @opac_count = "No"
       end
     end
 
