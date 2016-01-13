c9800cae4af21ff6333464bfa18b3734eb7df498
Katie Fritz <katie@katiemfritz.com>
2016-01-12 19:20:34 -0500

    OPAC and Kindle Unlimited are displaying results correctly, still very little formatting

--- app.rb
+++ app.rb
@@ -4,8 +4,9 @@ require 'nokogiri'
 
 class MyWebApp < Sinatra::Base
   get '/' do
-    @opac_records = opac("victor hugo")
-    erb :form
+    @opac_records = opac("octavia butler")
+    @ku_records = ku("octavia butler")
+    erb :results
   end
 
   helpers do
@@ -16,13 +17,31 @@ class MyWebApp < Sinatra::Base
       html.css("div.briefcitDetails").map do |book|
         title = book.css("h2.briefcitTitle a").first
         author = book.css(".briefcitAuthor").first
+        link = book.css(".briefcitActions a").first
         {
           title: title.text,
           author: author.text.gsub("\n", ""),
-          link: title.attr('href')
+          link: link.attr('href')
         }
       end
+    end
 
+    def ku(keyword)
+      keyword = keyword.split(/\s+/).join('+')
+      url = "http://www.amazon.com/s/?url=node%3D9069934011&field-keywords=#{keyword}"
+      page = RestClient.get(url)
+      html = Nokogiri::HTML(page)
+      count = html.css("h2#s-result-count").text
+      html.css("li.s-result-item").map do |book|
+        title = book.css("h2.s-access-title").first
+        author = book.css("span.a-size-small.a-color-secondary")[3]
+        link = book.css("a.a-link-normal").first
+        {
+          title: title.text,
+          author: author.text,
+          link: link.attr('href')
+        }
+      end
     end
 
   end
