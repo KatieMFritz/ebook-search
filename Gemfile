ffbcbe5065015e588c873b7b9b55bf15851c268c
Katie Fritz <katie@katiemfritz.com>
2016-01-14 21:15:00 -0500

    more bootstrap and dry up views with sinatra-contentfor

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ source 'https://rubygems.org'
 gem 'sinatra', '~> 1.4.6'
 gem 'nokogiri' #pull html from other sites
 gem 'rest-client' #api stuff
-gem 'sass'
+gem 'sass' #better css
+gem 'sinatra-contrib' #to allow content_for
 
 group :development do
   gem 'restart' #don't have to rackup every single time
--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
 require 'rest-client'
 require 'nokogiri'
+require "sinatra/content_for"
 
 require_relative 'helpers/kindle_unlimited_ebook_searcher'
 require_relative 'helpers/opac_ebook_searcher'
 
 class EbookSearcher < Sinatra::Base
+  helpers Sinatra::ContentFor
   get '/' do
+    erb :home
+  end
+  get '/results' do
     query = params[:query].to_s
     @kindle_unlimited_ebook_search = KindleUnlimitedEbookSearcher.new(query)
     @opac_ebook_search = OpacEbookSearcher.new(query)
